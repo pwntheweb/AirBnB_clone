cd0b9d5a1a35ca6d3205b81dd66ca55bbf204a23
pwntheweb <ezra005@proton.me>
2024-02-09 12:27:17 +0000

    console

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,13 @@ serializes instances to JSON file && deserialzes to instances from JSON file
 """
 import json
 import models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
 
 class FileStorage:
     """creating file storage"""
